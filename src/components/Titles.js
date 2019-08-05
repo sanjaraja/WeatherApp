1e45623c418e78711e3c3151a48a68ed09c6c219
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2019-08-05 15:59:19 -0700

    First commit to weather app

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Titles extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1> Weather Finder</h1>
+                <h2> Finding Condition</h2>
+            </div>
+        );
+    }
+}
+
+export default Titles; 
