691797392a4bb0e7fc9cbce852fe54d7985672a9
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2019-08-18 19:44:25 -0700

    Added final CSS modifciations for color coding display after country and city are entered

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,13 +2,30 @@ import React from "react";
 
 const Weather = (props) => {
     return (
-        <div>
-            {props.country && <p> Country: {props.country} </p>}
-            {props.city && <p> City: {props.city} </p>}
-            {props.temperature && <p> Temperature: {props.temperature} </p>}
-            {props.humidity && <p> Humidity: {props.humidity} % </p>}
-            {props.description && <p> Conditions: {props.description} </p>}
-            {props.error && <p> {props.error}</p>}
+        <div className="weather__info">
+            {
+                props.country && <p className="weather__key"> Country: 
+               <span className="weather__value"> {props.country} </span> </p>
+            }
+            {
+                props.city && <p className="weather__key"> City: 
+                <span className="weather__value"> {props.city}</span> </p>
+            }
+            {
+                props.temperature && <p className="weather__key"> Temperature: 
+                <span className="weather__value"> {props.temperature} </span> </p>
+            }
+            {
+                props.humidity && <p className="weather__key"> Humidity: 
+                <span className="weather__value"> {props.humidity}% </span> </p>
+            }
+            {
+                props.description && <p className="weather__key"> Conditions: 
+                <span className="weather__value"> {props.description} </span> </p>
+            }
+            {
+                props.error && <p className="weather__error"> {props.error}</p>
+            }
         </div>
     );
 }
